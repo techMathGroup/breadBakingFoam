a1ef713aa22662eae646642b59c54d7d37589dae
Tomas Hlavaty <t.hlavaty@outlook.com>
2025-11-02 09:56:42 +0100

    Installation script.

---
+++ install.sh
@@ -0,0 +1,24 @@
+########################################################################
+# Instalation script for bread baking simulations
+
+# Before run change the following variable to your path to solids4foam
+# and in applications/solvers/breadBakingFoam/Make/options
+pathToSolids4Foam="../solids4foam/"
+
+########################################################################
+
+# copy files to solids4foam 
+rsync -r solids4foamAddOns/src $pathToSolids4Foam
+
+# compile solids4foam
+prevPath=$(pwd)
+cd $pathToSolids4Foam
+./Allwmake
+cd $prevPath
+
+# compile breadBakingFoam
+cd applications/solvers/breadBakingFoam
+wmake
+cd $prevPath
+
+########################################################################
